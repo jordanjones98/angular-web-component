fef8d9cd11bf6036d9efc696a4333d11ab7a036c
JORDAN JONES <jjones42@wisc.edu>
2019-08-13 21:53:42 -0500

    Add basic material ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,26 @@
 import { NgModule, Injector } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { createCustomElement } from '@angular/elements';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MatCardModule } from '@angular/material';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 import { NgCardElementComponent } from './ng-card-element/ng-card-element.component';
 
 @NgModule({
-  imports: [BrowserModule,MatCardModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatIconModule,
+    MatTooltipModule
+  ],
   declarations: [NgCardElementComponent],
   entryComponents: [NgCardElementComponent] // use entryComponents array instaed of bootstrap because it will be added dynamically.
 })
--- src/app/ng-card-element/ng-card-element.component.ts
+++ src/app/ng-card-element/ng-card-element.component.ts
@@ -14,4 +14,5 @@ import {
 })
 export class NgCardElementComponent {
   @Input() title: string;
+  @Input() description: string;
 }
